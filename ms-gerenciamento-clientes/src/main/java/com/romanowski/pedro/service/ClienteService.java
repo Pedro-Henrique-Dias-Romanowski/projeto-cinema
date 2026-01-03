ea175d425ffcaf9e48a1f6a511ccf3e951c6a1de
Pedro Henrique Dias Romanowski <pedro.romanowski02@gmail.com>
2026-01-03 16:29:29 -0300

    implementado cadastro de cliente

--- ms-gerenciamento-clientes/src/main/java/com/romanowski/pedro/service/ClienteService.java
+++ ms-gerenciamento-clientes/src/main/java/com/romanowski/pedro/service/ClienteService.java
@@ -1,9 +1,45 @@
 package com.romanowski.pedro.service;
 
+import com.romanowski.pedro.entity.Cliente;
+import com.romanowski.pedro.repository.ClienteRepository;
+import com.romanowski.pedro.service.validation.ClienteValidation;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class ClienteService {
 
+    private final ClienteRepository clienteRepository;
 
+    private final ClienteValidation clienteValidation;
+
+    private final PasswordEncoder passwordEncoder;
+
+    public ClienteService(ClienteRepository clienteRepository, ClienteValidation clienteValidation, PasswordEncoder passwordEncoder) {
+        this.clienteRepository = clienteRepository;
+        this.clienteValidation = clienteValidation;
+        this.passwordEncoder = passwordEncoder;
+    }
+
+
+    public Cliente cadastrarCliente(Cliente cliente){
+        clienteValidation.validarCadastroCliente(cliente);
+        String senhaCriptografada = passwordEncoder.encode(cliente.getSenha());
+        cliente.setSenha(senhaCriptografada);
+        return clienteRepository.save(cliente);
+    }
+
+    public Cliente buscarClientePorId(Long id){
+        return null;
+    }
+
+    public List<Cliente> listarClientes(){
+        return null;
+    }
+
+    public void deletarCliente(Long id){
+
+    }
 }
