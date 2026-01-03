7cee49ba661ef0bba750f64d44f8ad26273364ec
Pedro Henrique Dias Romanowski <pedro.romanowski02@gmail.com>
2026-01-03 16:59:02 -0300

    removida exception nnao utilizada

---
+++ ms-gerenciamento-clientes/src/main/java/com/romanowski/pedro/exceptions/ListaClientesVaziaException.java
@@ -0,0 +1,7 @@
+package com.romanowski.pedro.exceptions;
+
+public class ListaClientesVaziaException extends BusinessException {
+    public ListaClientesVaziaException(String message) {
+        super(message);
+    }
+}
