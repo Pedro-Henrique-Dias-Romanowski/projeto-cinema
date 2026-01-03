3d20c9b0acfe54e51c95fbd3e30801dc5cdd1a2e
Pedro Henrique Dias Romanowski <pedro.romanowski02@gmail.com>
2026-01-03 16:28:56 -0300

    adicionando metodo de verificacao de email

--- ms-gerenciamento-clientes/src/main/java/com/romanowski/pedro/repository/ClienteRepository.java
+++ ms-gerenciamento-clientes/src/main/java/com/romanowski/pedro/repository/ClienteRepository.java
@@ -4,4 +4,6 @@ import com.romanowski.pedro.entity.Cliente;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ClienteRepository extends JpaRepository<Cliente, Long> {
+
+    Boolean existsByEmail(String email);
 }
