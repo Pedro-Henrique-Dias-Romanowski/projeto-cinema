1c23956a19d9d3908b6972404565490c97f06bb9
Pedro Henrique Dias Romanowski <pedro.romanowski02@gmail.com>
2026-01-08 20:26:19 -0300

    criada exception

---
+++ ms-gerenciamento-catalogo/src/main/java/com/romanowski/pedro/exceptions/ListaFilmesVaziaException.java
@@ -0,0 +1,7 @@
+package com.romanowski.pedro.exceptions;
+
+public class ListaFilmesVaziaException extends RuntimeException {
+    public ListaFilmesVaziaException(String message) {
+        super(message);
+    }
+}
