6f5640b4e6265a3922908e9393466b76f85beae4
Pedro Henrique Dias Romanowski <pedro.romanowski02@gmail.com>
2026-01-07 22:19:30 -0300

    criada entity

--- ms-gerenciamento-catalogo/src/main/java/com/romanowski/pedro/entity/Filme.java
+++ ms-gerenciamento-catalogo/src/main/java/com/romanowski/pedro/entity/Filme.java
@@ -1,4 +1,27 @@
 package com.romanowski.pedro.entity;
 
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDate;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode(of = "id")
+@Entity
+@Table(name = "filmes")
 public class Filme {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String nome;
+    private Integer duracao;
+    private String genero;
+    private String autor;
+    private LocalDate anoLancamento;
+
 }
