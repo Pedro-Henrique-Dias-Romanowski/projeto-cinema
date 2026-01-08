66460964335d7bd9547d3a209ea66f09eddf8e12
Pedro Henrique Dias Romanowski <pedro.romanowski02@gmail.com>
2026-01-08 19:24:39 -0300

    adicionada exceptions

---
+++ ms-gerenciamento-catalogo/src/main/java/com/romanowski/pedro/exceptions/FilmeExistenteException.java
@@ -0,0 +1,7 @@
+package com.romanowski.pedro.exceptions;
+
+public class FilmeExistenteException extends BusinessException {
+    public FilmeExistenteException(String message) {
+        super(message);
+    }
+}
