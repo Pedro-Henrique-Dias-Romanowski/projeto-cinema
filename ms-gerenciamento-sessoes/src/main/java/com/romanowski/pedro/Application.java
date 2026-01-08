6b37f0123798a87f578d4029bff226b7a76420bd
Pedro Henrique Dias Romanowski <pedro.romanowski02@gmail.com>
2026-01-07 21:33:12 -0300

    adicionado global.properties

--- ms-gerenciamento-sessoes/src/main/java/com/romanowski/pedro/Application.java
+++ ms-gerenciamento-sessoes/src/main/java/com/romanowski/pedro/Application.java
@@ -2,7 +2,9 @@ package com.romanowski.pedro;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
 
+@PropertySource("classpath:messages/global.properties")
 @SpringBootApplication
 public class Application {
 
