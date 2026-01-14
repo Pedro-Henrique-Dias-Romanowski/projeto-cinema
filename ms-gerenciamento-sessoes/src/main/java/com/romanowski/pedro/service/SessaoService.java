5bbf5781ddb35b0a094e832ea9113e37144fffb0
Pedro Henrique Dias Romanowski <pedro.romanowski02@gmail.com>
2026-01-13 21:14:38 -0300

    implementada assinatura metodos service

--- ms-gerenciamento-sessoes/src/main/java/com/romanowski/pedro/service/SessaoService.java
+++ ms-gerenciamento-sessoes/src/main/java/com/romanowski/pedro/service/SessaoService.java
@@ -1,7 +1,31 @@
 package com.romanowski.pedro.service;
 
+import com.romanowski.pedro.entity.Sessao;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class SessaoService {
+
+    public Sessao fazerReservaSessao(Sessao sessao){
+        return null;
+    }
+
+    public Sessao confirmarReservaSessao(Long id){
+        return null;
+    }
+
+    public List<Sessao> listarSessoes(){
+        return List.of();
+    }
+
+    public Optional<Sessao> procurarSessaoPorId(Long id){
+        return Optional.empty();
+    }
+
+    public void cancelarReservaSessao(Long id){
+
+    }
 }
