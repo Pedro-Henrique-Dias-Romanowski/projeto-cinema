1e3d24d160604f65e9559a7c7414831ccda8323b
Pedro Henrique Dias Romanowski <pedro.romanowski02@gmail.com>
2026-01-13 21:14:04 -0300

    implementado controller e swagger

--- ms-gerenciamento-sessoes/src/main/java/com/romanowski/pedro/controller/SessaoController.java
+++ ms-gerenciamento-sessoes/src/main/java/com/romanowski/pedro/controller/SessaoController.java
@@ -1,9 +1,61 @@
 package com.romanowski.pedro.controller;
 
+import com.romanowski.pedro.controller.swagger.SwaggerSessaoController;
+import com.romanowski.pedro.dto.request.SessaoRequestDTO;
+import com.romanowski.pedro.dto.response.SessaoResponseDTO;
+import com.romanowski.pedro.entity.Sessao;
+import com.romanowski.pedro.mapper.SessaoMapper;
+import com.romanowski.pedro.service.SessaoService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+import java.util.Optional;
+
 @RestController
 @RequestMapping("/v1")
-public class SessaoController {
+public class SessaoController implements SwaggerSessaoController {
+
+    private final SessaoMapper sessaoMapper;
+    private final SessaoService sessaoService;
+
+    public SessaoController(SessaoMapper sessaoMapper, SessaoService sessaoService) {
+        this.sessaoMapper = sessaoMapper;
+        this.sessaoService = sessaoService;
+    }
+
+
+    @Override
+    public ResponseEntity<SessaoResponseDTO> fazerReservaSessao(SessaoRequestDTO sessaoRequestDTO) {
+        Sessao sessao = sessaoMapper.toEntity(sessaoRequestDTO);
+        Sessao sessaoSalva = sessaoService.fazerReservaSessao(sessao);
+        return ResponseEntity.status(HttpStatus.OK).body(sessaoMapper.toResponseDTO(sessaoSalva));
+    }
+
+    @Override
+    public ResponseEntity<SessaoResponseDTO> confirmarReservaSessao(Long id) {
+        Sessao sessaoConfirmada = sessaoService.confirmarReservaSessao(id);
+        return ResponseEntity.status(HttpStatus.OK).body(sessaoMapper.toResponseDTO(sessaoConfirmada));
+    }
+
+    @Override
+    public ResponseEntity<List<SessaoResponseDTO>> listarSessoes() {
+        List<Sessao> sessoes = sessaoService.listarSessoes();
+        List<SessaoResponseDTO> sessoesResponseDTO = sessoes.stream().map(sessaoMapper::toResponseDTO).toList();
+        return ResponseEntity.status(HttpStatus.OK).body(sessoesResponseDTO);
+    }
+
+    @Override
+    public ResponseEntity<SessaoResponseDTO> procurarSessaoPorId(Long id) {
+        Optional<Sessao> sessaoEncontrada = sessaoService.procurarSessaoPorId(id);
+        return ResponseEntity.status(HttpStatus.OK).body(sessaoMapper.toResponseDTO(sessaoEncontrada.orElse(null)));
+    }
+
+    @Override
+    public ResponseEntity<Void> cancelarReservaSessao(Long id) {
+        sessaoService.cancelarReservaSessao(id);
+        return ResponseEntity.status(HttpStatus.NO_CONTENT).body(null);
+    }
 }
--- ms-gerenciamento-sessoes/src/main/java/com/romanowski/pedro/controller/swagger/SwaggerSessaoController.java
+++ ms-gerenciamento-sessoes/src/main/java/com/romanowski/pedro/controller/swagger/SwaggerSessaoController.java
@@ -1,4 +1,27 @@
 package com.romanowski.pedro.controller.swagger;
 
+import com.romanowski.pedro.dto.request.SessaoRequestDTO;
+import com.romanowski.pedro.dto.response.SessaoResponseDTO;
+import jakarta.validation.Valid;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
 public interface SwaggerSessaoController {
+
+    @PostMapping("/sessoes")
+    ResponseEntity<SessaoResponseDTO> fazerReservaSessao(@Valid @RequestBody SessaoRequestDTO sessaoRequestDTO);
+
+    @PostMapping("/sessoes/confirmar/{id}")
+    ResponseEntity<SessaoResponseDTO> confirmarReservaSessao(@PathVariable Long id);
+
+    @GetMapping("/sessoes")
+    ResponseEntity<List<SessaoResponseDTO>> listarSessoes();
+
+    @GetMapping("/sessoes/{id}")
+    ResponseEntity<SessaoResponseDTO> procurarSessaoPorId(Long id);
+
+    @DeleteMapping("sessoes/{id}")
+    ResponseEntity<Void> cancelarReservaSessao(@PathVariable Long id);
 }
