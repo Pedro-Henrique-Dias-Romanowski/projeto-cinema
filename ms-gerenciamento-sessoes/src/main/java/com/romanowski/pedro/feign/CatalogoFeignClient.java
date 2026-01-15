72252dbd157b1d3b4334dfeaee04583de737085e
Pedro Henrique Dias Romanowski <pedro.romanowski02@gmail.com>
2026-01-14 21:33:58 -0300

    adicionado feign clients

--- ms-gerenciamento-sessoes/src/main/java/com/romanowski/pedro/feign/CatalogoFeignClient.java
+++ ms-gerenciamento-sessoes/src/main/java/com/romanowski/pedro/feign/CatalogoFeignClient.java
@@ -1,4 +1,15 @@
 package com.romanowski.pedro.feign;
 
+import com.romanowski.pedro.config.FeignInterceptor;
+import com.romanowski.pedro.dto.response.FilmeResponseDTO;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.Optional;
+
+@FeignClient(name = "catalogo-service", url = "${URL_CATALOGO_SERVICE}", configuration = FeignInterceptor.class)
 public interface CatalogoFeignClient {
+
+    @GetMapping("v1/filmes/titulo")
+    Optional<FilmeResponseDTO> obterFilmePorTitulo();
 }
