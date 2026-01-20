695e59bc632edf1548d6493710fcd05a840427c8
Pedro Henrique Dias Romanowski <pedro.romanowski02@gmail.com>
2026-01-19 22:27:06 -0300

    adicionada exceptions

---
+++ ms-gerenciamento-sessoes/src/main/java/com/romanowski/pedro/exceptions/ListaSessoesVaziaException.java
@@ -0,0 +1,7 @@
+package com.romanowski.pedro.exceptions;
+
+public class ListaSessoesVaziaException extends BusinessException {
+    public ListaSessoesVaziaException(String message) {
+        super(message);
+    }
+}
---
+++ ms-gerenciamento-sessoes/src/main/java/com/romanowski/pedro/exceptions/SessaoExistenteException.java
@@ -0,0 +1,7 @@
+package com.romanowski.pedro.exceptions;
+
+public class SessaoExistenteException extends BusinessException{
+    public SessaoExistenteException(String message) {
+        super(message);
+    }
+}
