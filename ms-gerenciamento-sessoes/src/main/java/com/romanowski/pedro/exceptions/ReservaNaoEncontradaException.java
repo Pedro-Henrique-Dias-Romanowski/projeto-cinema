7f8b62c6c8b01b2b23ffcc5ba66628d9eae7630f
Pedro Henrique Dias Romanowski <pedro.romanowski02@gmail.com>
2026-01-26 23:19:30 -0300

    adicionada exception

---
+++ ms-gerenciamento-sessoes/src/main/java/com/romanowski/pedro/exceptions/ReservaNaoEncontradaException.java
@@ -0,0 +1,7 @@
+package com.romanowski.pedro.exceptions;
+
+public class ReservaNaoEncontradaException extends BusinessException {
+    public ReservaNaoEncontradaException(String message) {
+        super(message);
+    }
+}
