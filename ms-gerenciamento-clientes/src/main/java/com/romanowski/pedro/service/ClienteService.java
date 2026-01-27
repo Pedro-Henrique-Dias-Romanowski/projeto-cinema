c749cf785587e9100c4b176ad32db89c3f098d53
Pedro Henrique Dias Romanowski <pedro.romanowski02@gmail.com>
2026-01-27 20:36:54 -0300

    adicionado logs

--- ms-gerenciamento-clientes/src/main/java/com/romanowski/pedro/service/ClienteService.java
+++ ms-gerenciamento-clientes/src/main/java/com/romanowski/pedro/service/ClienteService.java
@@ -1,8 +1,11 @@
 package com.romanowski.pedro.service;
 
+import com.romanowski.pedro.Application;
 import com.romanowski.pedro.entity.Cliente;
 import com.romanowski.pedro.repository.ClienteRepository;
 import com.romanowski.pedro.service.validation.ClienteValidation;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
@@ -11,6 +14,8 @@ import java.util.Optional;
 
 @Service
 public class ClienteService {
+
+    private static final Logger logger = LoggerFactory.getLogger(ClienteService.class);
 
     private final ClienteRepository clienteRepository;
 
@@ -26,6 +31,7 @@ public class ClienteService {
 
 
     public Cliente cadastrarCliente(Cliente cliente){
+        logger.info("Iniciando cadastro do cliente com email: {}", cliente.getEmail());
         clienteValidation.validarCadastroCliente(cliente);
         String senhaCriptografada = passwordEncoder.encode(cliente.getSenha());
         cliente.setSenha(senhaCriptografada);
@@ -33,15 +39,18 @@ public class ClienteService {
     }
 
     public Optional<Cliente> buscarClientePorId(Long id){
+        logger.info("Iniciando busca do cliente com id: {}", id);
         return clienteRepository.findById(id);
     }
 
     public List<Cliente> listarClientes(){
+        logger.info("Iniciando listagem de clientes");
         clienteValidation.validarListagemClientes();
         return clienteRepository.findAll();
     }
 
     public void deletarCliente(Long id){
+        logger.info("Iniciando remoção do cliente com id: {}", id);
         clienteValidation.validarBuscaPorCliente(id);
         clienteRepository.deleteById(id);
     }
--- ms-gerenciamento-clientes/src/main/java/com/romanowski/pedro/service/validation/ClienteValidation.java
+++ ms-gerenciamento-clientes/src/main/java/com/romanowski/pedro/service/validation/ClienteValidation.java
@@ -6,6 +6,8 @@ import com.romanowski.pedro.exceptions.EmailExistenteException;
 import com.romanowski.pedro.exceptions.ListaClientesVaziaException;
 import com.romanowski.pedro.exceptions.SenhaInvalidaExcpetion;
 import com.romanowski.pedro.repository.ClienteRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
@@ -13,6 +15,8 @@ import org.springframework.stereotype.Component;
 public class ClienteValidation {
 
     private final ClienteRepository clienteRepository;
+
+    private static final Logger logger = LoggerFactory.getLogger(ClienteValidation.class);
 
     @Value("${cliente.email.existente}")
     private String mensagemEmailExistente;
@@ -37,12 +41,14 @@ public class ClienteValidation {
 
     public void validarListagemClientes(){
         if (clienteRepository.findAll().isEmpty()) {
+            logger.error("Nenhum cliente encontrado");
             throw new ListaClientesVaziaException(mensagemListaVazia);
         }
     }
 
     public void validarBuscaPorCliente(Long id){
         if (clienteRepository.findById(id).isEmpty()){
+            logger.error("Cliente com id {} não encontrado", id);
             throw new ClienteInexistenteException(mensagemClienteInexistente);
         }
     }
@@ -50,12 +56,14 @@ public class ClienteValidation {
 
     private void validarExistenciaEmail(String email){
         if (clienteRepository.existsByEmail(email)){
+            logger.info("Cliente com email {} encontrado", email);
             throw new EmailExistenteException(mensagemEmailExistente);
         }
     }
 
     private void validarSenhaCliente(String senha){
         if (senha.length() <= 5 || senha.length() > 15){
+            logger.error("Senha inválida");
             throw new SenhaInvalidaExcpetion(mensagemSenhaInvalida);
         }
     }
