baa6605dd15dc2498bd4219c81f7580600ab9bef
Pedro Henrique Dias Romanowski <pedro.romanowski02@gmail.com>
2026-01-28 20:26:43 -0300

    adicionado feign client

--- ms-gerenciamento-clientes/src/main/java/com/romanowski/pedro/Application.java
+++ ms-gerenciamento-clientes/src/main/java/com/romanowski/pedro/Application.java
@@ -2,9 +2,11 @@ package com.romanowski.pedro;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.PropertySource;
 
 @PropertySource("classpath:messages/global.properties")
+@EnableFeignClients
 @SpringBootApplication
 public class Application {
 
---
+++ ms-gerenciamento-clientes/src/main/java/com/romanowski/pedro/feign/ReservaFeignClient.java
@@ -0,0 +1,13 @@
+package com.romanowski.pedro.feign;
+
+import com.romanowski.pedro.dto.response.ReservaResponseDTO;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@FeignClient(name = "ms-gerenciamento-sessoes", url = "${URL_RESERVA_SERVICE}")
+public interface ReservaFeignClient {
+
+    @GetMapping("v1/reservas/{idCliente}/{idReserva}")
+    ReservaResponseDTO buscarReservaPorId(@PathVariable Long idCliente, @PathVariable Long idReserva);
+}
