d85bdba6757fb7efaf129b9b62ac00d21817f8cd
Pedro Henrique Dias Romanowski <pedro.romanowski02@gmail.com>
2026-01-28 20:26:08 -0300

    adicionada exceptions

---
+++ ms-gerenciamento-clientes/src/main/java/com/romanowski/pedro/exceptions/ReservaInexistenteException.java
@@ -0,0 +1,7 @@
+package com.romanowski.pedro.exceptions;
+
+public class ReservaInexistenteException extends BusinessException {
+    public ReservaInexistenteException(String message) {
+        super(message);
+    }
+}
---
+++ ms-gerenciamento-clientes/src/main/java/com/romanowski/pedro/exceptions/SaldoInsuficienteException.java
@@ -0,0 +1,7 @@
+package com.romanowski.pedro.exceptions;
+
+public class SaldoInsuficienteException extends BusinessException {
+    public SaldoInsuficienteException(String message) {
+        super(message);
+    }
+}
