ed7c95bad68d21f64c5a03aa075dc1d1c789e96e
Pedro Henrique Dias Romanowski <pedro.romanowski02@gmail.com>
2026-02-10 23:39:14 -0300

    adicionado swagger aplicacao

--- ms-gerenciamento-sessoes/src/main/java/com/romanowski/pedro/controller/swagger/SwaggerReservaController.java
+++ ms-gerenciamento-sessoes/src/main/java/com/romanowski/pedro/controller/swagger/SwaggerReservaController.java
@@ -1,6 +1,8 @@
 package com.romanowski.pedro.controller.swagger;
 
 import com.romanowski.pedro.dto.response.ReservaResponseDTO;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,17 +11,23 @@ import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.List;
 
+@Tag(name = "Reserva", description = "Operações disponíveis para as reservas de sessões do cinema.")
 public interface SwaggerReservaController {
 
+    @Operation(summary = "Criar reserva", description = "Permite a criação de uma nova reserva para uma sessão específica por um cliente.")
     @PostMapping("/reservas/{idCliente}/{idSessao}" )
     ResponseEntity<ReservaResponseDTO> criarReserva(@PathVariable Long idCliente, @PathVariable Long idSessao);
 
+
+    @Operation(summary = "Listar reservas", description = "Permite a listagem de todas as reservas feitas por um cliente específico.")
     @GetMapping("/reservas/{idCliente}")
     ResponseEntity<List<ReservaResponseDTO>> listarReservas(@PathVariable Long idCliente);
 
+    @Operation(summary = "Buscar reserva por id", description = "Permite a busca de uma reserva específica por se ID e o ID do cliente.")
     @GetMapping("/reservas/{idCliente}/{idReserva}")
     ResponseEntity<ReservaResponseDTO> buscarReservaPorId(@PathVariable Long idCliente, @PathVariable Long idReserva);
 
+    @Operation(summary = "Cancelamento de reserva", description = "Permite o cancelamento (exclusão lógica) de uma reserva específica por seu ID e o ID do cliente.")
     @DeleteMapping("/reservas/{idCliente}/{idReserva}")
     ResponseEntity<Void> cancelarReserva(@PathVariable Long idCliente, @PathVariable Long idReserva);
 }
--- ms-gerenciamento-sessoes/src/main/java/com/romanowski/pedro/controller/swagger/SwaggerSessaoController.java
+++ ms-gerenciamento-sessoes/src/main/java/com/romanowski/pedro/controller/swagger/SwaggerSessaoController.java
@@ -2,24 +2,31 @@ package com.romanowski.pedro.controller.swagger;
 
 import com.romanowski.pedro.dto.request.SessaoRequestDTO;
 import com.romanowski.pedro.dto.response.SessaoResponseDTO;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@Tag(name = "Sessão", description = "Operações disponíveis para as sessões do cinema.")
 public interface SwaggerSessaoController {
 
 
+    @Operation(summary = "Cadastrar sessão", description = "Permite o cadastro de uma nova sessão no cinema.")
     @PostMapping("/sessoes")
     ResponseEntity<SessaoResponseDTO> cadastrarSessao(@Valid @RequestBody SessaoRequestDTO sessaoRequestDTO);
 
+    @Operation(summary = "Listar sessões", description = "Permite a listagem de todas as sessões disponíveis no cinema.")
     @GetMapping("/sessoes")
     ResponseEntity<List<SessaoResponseDTO>> listarSessoes();
 
+    @Operation(summary = "Buscar sessão por ID", description = "Permite a busca de uma sessão específica por seu ID.")
     @GetMapping("/sessoes/{id}")
     ResponseEntity<SessaoResponseDTO> procurarSessaoPorId(@PathVariable Long id);
 
+    @Operation(summary = "Cancelar sessão", description = "Permite o cancelamento (exclusão lógica) de uma sessão específica por seu ID.")
     @DeleteMapping("sessoes/{idSessao}")
     ResponseEntity<Void> cancelarSessao(@PathVariable Long idSessao);
 }
