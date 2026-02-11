c32ed2feddec617fc306153e0d604376779041a8
Pedro Henrique Dias Romanowski <pedro.romanowski02@gmail.com>
2026-02-10 23:30:08 -0300

    adicionado swagger da aplicacao

---
+++ ms-gerenciamento-catalogo/src/main/java/com/romanowski/pedro/config/OpenApiConfiguration.java
@@ -0,0 +1,22 @@
+package com.romanowski.pedro.config;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+
+@OpenAPIDefinition(info =
+@io.swagger.v3.oas.annotations.info.Info(title = "ms-gerenciamento-catalogo API",
+        version = "v1",
+        description = "Documentation of ms-gerenciamento-catalogo API"))
+public class OpenApiConfiguration {
+
+    @Bean
+    public OpenAPI bibliotecaVirtualOpenAPI(){
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Cinecom")
+                        .description("Documentação da ms-gerenciamento-catalogo")
+                        .version("1.0.0"));
+    }
+}
--- ms-gerenciamento-catalogo/src/main/java/com/romanowski/pedro/controller/swagger/SwaggerFilmeController.java
+++ ms-gerenciamento-catalogo/src/main/java/com/romanowski/pedro/controller/swagger/SwaggerFilmeController.java
@@ -3,29 +3,38 @@ package com.romanowski.pedro.controller.swagger;
 import com.romanowski.pedro.dto.request.FilmeAtualizacaoRequestDTO;
 import com.romanowski.pedro.dto.request.FilmeRequestDTO;
 import com.romanowski.pedro.dto.response.FilmeResponseDTO;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@Tag(name = "Filme", description = "Operações disponíveis para os filmes do cinema.")
 public interface SwaggerFilmeController {
 
+    @Operation(summary = "Cadastrar filme", description = "Permite o cadastro de um novo filme na biblioteca.")
     @PostMapping("/filmes")
     ResponseEntity<FilmeResponseDTO> cadastrarFilme(@Valid @RequestBody FilmeRequestDTO filmeRequestDTO);
 
+    @Operation(summary = "Listar filmes", description = "Permite a listagem de todos os filmes disponíveis na biblioteca.")
     @GetMapping("/filmes")
     ResponseEntity<List<FilmeResponseDTO>> listarFilmes();
 
+    @Operation(summary = "Buscar filme por id", description = "Permite a busca de um filme específico por seu ID.")
     @GetMapping("/filmes/{id}")
     ResponseEntity<FilmeResponseDTO> buscarFilmePorId(@PathVariable Long id);
 
+    @Operation(summary = "Buscar filme por título", description = "Permite a busca de um filme específico por seu título.")
     @GetMapping("/filmes/titulo")
     ResponseEntity<FilmeResponseDTO> buscarFilmePorTitulo(@RequestHeader String titulo);
 
+    @Operation(summary = "Atualizar filme", description = "Permite a atualização dos dados de um filme específico por seu ID.")
     @PatchMapping("/filmes/{id}")
     ResponseEntity<FilmeResponseDTO> atualizarFilme(@PathVariable Long id, @RequestBody FilmeAtualizacaoRequestDTO filmeAtualizacaoRequestDTO);
 
+    @Operation(summary = "Exclusão de um filme", description = "Permite a exclusão de um filme específico por seu ID.")
     @DeleteMapping("/filmes/{id}")
     ResponseEntity<Void> deletarFilme(@PathVariable Long id);
 }
