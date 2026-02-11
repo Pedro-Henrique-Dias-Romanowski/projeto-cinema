bca734a267b697b42a348ab50c34eae5a6256e79
Pedro Henrique Dias Romanowski <pedro.romanowski02@gmail.com>
2026-02-10 23:40:17 -0300

    alterada documentacao swagger

--- ms-gerenciamento-clientes/src/main/java/com/romanowski/pedro/controller/swagger/ClienteControllerSwagger.java
+++ ms-gerenciamento-clientes/src/main/java/com/romanowski/pedro/controller/swagger/ClienteControllerSwagger.java
@@ -2,23 +2,30 @@ package com.romanowski.pedro.controller.swagger;
 
 import com.romanowski.pedro.dto.request.ClienteRequestDTO;
 import com.romanowski.pedro.dto.response.ClienteResponseDTO;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@Tag(name = "Cliente", description = "Operações disponíveis para clientes do cinema.")
 public interface ClienteControllerSwagger {
 
+    @Operation(summary = "Cadastrar cliente", description = "Permite o cadastro de um novo cliente no cinema.")
     @PostMapping("/clientes")
     ResponseEntity<ClienteResponseDTO> cadastrarCliente(@Valid @RequestBody ClienteRequestDTO clienteRequestDTO);
 
+    @Operation(summary = "Cadastrar cliente", description = "Permite listar os clientes que estão cadastrados dentro do cinema.")
     @GetMapping("/clientes")
     ResponseEntity<List<ClienteResponseDTO>> listarClientes();
 
+    @Operation(summary = "Cadastrar cliente", description = "Permite a busca por um cliente específico através do seu ID.")
     @GetMapping("/clientes/{id}")
     ResponseEntity<ClienteResponseDTO> buscarClientePorId(@PathVariable @Valid Long id);
 
+    @Operation(summary = "Cadastrar cliente", description = "Permite a exclusão de um cliente específico através do seu ID.")
     @DeleteMapping("/clientes/{id}")
     ResponseEntity<Void> deletarCliente(@PathVariable Long id);
 }
--- ms-gerenciamento-clientes/src/main/java/com/romanowski/pedro/controller/swagger/PagamentoControllerSwagger.java
+++ ms-gerenciamento-clientes/src/main/java/com/romanowski/pedro/controller/swagger/PagamentoControllerSwagger.java
@@ -1,14 +1,18 @@
 package com.romanowski.pedro.controller.swagger;
 
 import com.romanowski.pedro.dto.request.PagamentoRequestDTO;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
+@Tag(name = "Pagamento", description = "Possíveis transações relacionadas a pagamento no cinema.")
 public interface PagamentoControllerSwagger {
 
+    @Operation(summary = "Realizar o pagamento", description = "Permite a realização do pagamento de uma reserva feita por um cliente.")
     @PostMapping("/pagamentos/{idCliente}/{idReserva}")
     ResponseEntity<Void> realizarPagamento(@PathVariable Long idCliente, @PathVariable Long idReserva, @Valid @RequestBody PagamentoRequestDTO pagamentoRequestDTO);
 }
