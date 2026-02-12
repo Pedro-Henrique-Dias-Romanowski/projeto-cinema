465bc563d0aa4113c3582dadaaa4783efa612ead
Pedro Henrique Dias Romanowski <pedro.romanowski02@gmail.com>
2026-02-12 20:38:41 -0300

    adicionada mensagens

--- ms-autenticacao-cinema/src/main/java/com/romanowski/pedro/Application.java
+++ ms-autenticacao-cinema/src/main/java/com/romanowski/pedro/Application.java
@@ -3,12 +3,14 @@ package com.romanowski.pedro;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.PropertySource;
 
+@PropertySource("classpath:messages/global.properties")
 @SpringBootApplication
 @EnableFeignClients
 public class Application {
 
-    public static void main(String[] args) {
+    static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
 
