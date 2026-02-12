428bb96f63b56bc2457261d4237d1dedcda2d700
Pedro Henrique Dias Romanowski <pedro.romanowski02@gmail.com>
2026-02-12 20:39:13 -0300

    liberado requisicoes para login e cadastro de clientes

--- ms-autenticacao-cinema/src/main/java/com/romanowski/pedro/config/SecurityConfiguration.java
+++ ms-autenticacao-cinema/src/main/java/com/romanowski/pedro/config/SecurityConfiguration.java
@@ -2,15 +2,33 @@ package com.romanowski.pedro.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 public class SecurityConfiguration {
     // todo implementar autorização de requisições e adição de filtro
 
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http
+                .csrf(AbstractHttpConfigurer::disable)
+                .sessionManagement(sm -> sm.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/v1/clientes/login", "/v1/auth/clientes").permitAll()
+                        .anyRequest().authenticated()
+                )
+                //.addFilterBefore(securityFilter, UsernamePasswordAuthenticationFilter.class)
+                .build();
+    }
 
     @Bean
     public PasswordEncoder passwordEncoder() {
