ab15915d22f23ac00f9fc9fc47ab9a401b602856
Pedro Henrique Dias Romanowski <pedro.romanowski02@gmail.com>
2026-02-17 17:28:05 -0300

    alterado configuracao swagger

--- ms-gerenciamento-sessoes/src/main/java/com/romanowski/pedro/config/OpenApiConfiguration.java
+++ ms-gerenciamento-sessoes/src/main/java/com/romanowski/pedro/config/OpenApiConfiguration.java
@@ -1,22 +1,35 @@
 package com.romanowski.pedro.config;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
 @OpenAPIDefinition(info =
 @io.swagger.v3.oas.annotations.info.Info(title = "ms-gerenciamento-sessoes API",
         version = "v1",
-        description = "Documentation of ms-gerenciamento-sessoes API"))
+        description = "Documentação da Microserviço de Gerenciamento de Sessões e Reservas do Cinema"))
 public class OpenApiConfiguration {
 
     @Bean
-    public OpenAPI bibliotecaVirtualOpenAPI(){
+    public OpenAPI bibliotecaVirtualOpenAPI() {
         return new OpenAPI()
-                .info(new Info()
-                        .title("Cinecom")
-                        .description("Documentação da ms-gerenciamento-sessoes")
-                        .version("1.0.0"));
+                .info(new io.swagger.v3.oas.models.info.Info()
+                        .title("Cinecom - Gerenciamento Sessões e Reservas")
+                        .description("Documentação do ms-gerenciamento-sessoes")
+                        .version("1.0.0"))
+                .addSecurityItem(new SecurityRequirement().addList("bearer-jwt"))
+                .components(new Components()
+                        .addSecuritySchemes("bearer-jwt", new SecurityScheme()
+                                .type(SecurityScheme.Type.HTTP)
+                                .scheme("bearer")
+                                .bearerFormat("JWT")
+                                .in(SecurityScheme.In.HEADER)
+                                .name("Authorization")));
     }
 }
