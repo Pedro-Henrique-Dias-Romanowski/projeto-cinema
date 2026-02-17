5ebc138691d678ff925006a3de209966bd9a9d73
Pedro Henrique Dias Romanowski <pedro.romanowski02@gmail.com>
2026-02-17 14:37:51 -0300

    adicionado DTO especifico para feign client ms-gerenciamento-clientes

---
+++ ms-autenticacao-cinema/src/main/java/com/romanowski/pedro/dto/request/CadastroFeignClientRequestDTO.java
@@ -0,0 +1,27 @@
+package com.romanowski.pedro.dto.request;
+
+import jakarta.validation.constraints.*;
+
+import java.util.UUID;
+
+public record CadastroFeignClientRequestDTO(
+
+        @NotNull
+        UUID id,
+
+        @NotBlank
+        String nome,
+
+        @Email
+        @NotBlank
+        String email,
+
+        @NotBlank
+        String senha,
+
+        @NotNull
+        @PositiveOrZero
+        @DecimalMax("1000.0")
+        Double saldo
+) {
+}
