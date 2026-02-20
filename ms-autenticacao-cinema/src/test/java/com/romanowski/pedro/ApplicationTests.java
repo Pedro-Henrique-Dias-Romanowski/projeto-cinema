bcfddaabd78b07787e989cd81f69104101ff52b5
Pedro Henrique Dias Romanowski <pedro.romanowski02@gmail.com>
2026-02-19 23:24:54 -0300

    configuracao profile test

--- ms-autenticacao-cinema/src/test/java/com/romanowski/pedro/ApplicationTests.java
+++ ms-autenticacao-cinema/src/test/java/com/romanowski/pedro/ApplicationTests.java
@@ -2,10 +2,10 @@ package com.romanowski.pedro;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.annotation.Profile;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
-@Profile("test")
+@ActiveProfiles("test")
 class ApplicationTests {
 
     @Test
