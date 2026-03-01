79d819be5d66080b83ea851c5b7fc943a8f82be6
Pedro Henrique Dias Romanowski <pedro.romanowski02@gmail.com>
2026-03-01 20:30:04 -0300

    adicionado service discovery com eureka

---
+++ service-discovery-cinema/src/test/java/com/romanowski/pedro/ServiceDiscoveryCinemaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.romanowski.pedro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceDiscoveryCinemaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
